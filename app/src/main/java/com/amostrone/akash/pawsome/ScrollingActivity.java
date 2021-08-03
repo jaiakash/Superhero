5bafda98845d0b49a582e83104eee9490d74c6de
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-03 14:02:44 +0530

    Added scrollview (xml)

--- app/src/main/java/com/amostrone/akash/pawsome/ScrollingActivity.java
+++ app/src/main/java/com/amostrone/akash/pawsome/ScrollingActivity.java
@@ -3,13 +3,10 @@ package com.amostrone.akash.pawsome;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 

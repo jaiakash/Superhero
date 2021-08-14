d179da00de2736aed79b1dfc8f6ea1c86e6c8af5
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-15 00:19:17 +0530

    Deleted boilerplate

--- app/src/main/java/com/amostrone/akash/superhero/ScrollingActivity.java
+++ app/src/main/java/com/amostrone/akash/superhero/ScrollingActivity.java
@@ -1,30 +1,26 @@
 package com.amostrone.akash.superhero;
 
 import android.os.Bundle;
-
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.amostrone.akash.superhero.databinding.ActivityScrollingBinding;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    private ActivityScrollingBinding binding;
+private ActivityScrollingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+     binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
@@ -40,8 +36,7 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
+@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_scrolling, menu);

56bbc0b0eeafbba5caca5ae13d3b26899e6fd3df
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-15 00:26:29 +0530

    Deleted boilerplate 2/2

--- app/src/main/java/com/amostrone/akash/superhero/ScrollingActivity.java
+++ app/src/main/java/com/amostrone/akash/superhero/ScrollingActivity.java
@@ -1,15 +1,14 @@
 package com.amostrone.akash.superhero;
 
 import android.os.Bundle;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.amostrone.akash.superhero.databinding.ActivityScrollingBinding;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -26,15 +25,6 @@ private ActivityScrollingBinding binding;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 @Override
     public boolean onCreateOptionsMenu(Menu menu) {
